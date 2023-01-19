7271d1749debd6381fc8dfd0a109214ba9855d7c
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2023-01-19 10:36:27 -0500

    Se filtraron los post por categoría.
    Se filtraron los post por categoría.

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -33,7 +33,14 @@ class PostController extends Controller
 
     public function category(Category  $category){
 
-        return $category;
+        $posts= Post::where('category_id',$category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(6);
+                        
+
+
+        return view('posts.category', compact('posts','category'));
 
     }
 }
