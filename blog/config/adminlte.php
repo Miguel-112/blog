3f8bc55277658473e1dac5189c91d0f2cdee3234
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2023-03-13 13:26:24 -0500

    Se culminó la gestión de roles.
    Se culminó la gestión de roles.

--- blog/config/adminlte.php
+++ blog/config/adminlte.php
@@ -325,6 +325,13 @@ return [
             'can'  => 'admin.users.index',
 
         ],
+        [
+            'text'        => 'Lista de Roles',
+            'route'         => 'admin.roles.index',
+            'icon'        => 'fas fa-users-cog fa.fw ',
+            'can'  => 'admin.users.index',
+
+        ],
        
         [
             'text' => 'categorias',
